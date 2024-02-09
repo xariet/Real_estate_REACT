18c8dc469b96b4f51a04d2852a1b7804e8201b64
marina <xariet@hotmail.com>
2024-02-09 21:34:28 +1100

    final

--- OneDrive/Desktop/react-test/hera-real-estate/src/Pages/Home.js
+++ OneDrive/Desktop/react-test/hera-real-estate/src/Pages/Home.js
@@ -1,7 +1,17 @@
+
+import './styles/Home.css'
+
 function Home(){
 return(
-    <div>
-        This is a home page
+    <div className="home-div">
+       <section className="main">
+        <h1>Where Home Find Their Stories</h1>
+        <p className="homeP">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, error
+            maxime debitis veniam dolor, assumenda hic vitae id nihil perferendis
+            facilis! Sed autem quas ipsam, eos optio natus dolorem corporis.
+        </p>
+       </section>
     </div>
 )
 }
